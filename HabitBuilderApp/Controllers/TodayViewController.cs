6db17d8eb39726670102c82839fbb1a53e16de1b
ArturWD <shestakovartur2.0@gmail.com>
2019-02-05 20:31:24 +0500

    bg and favicon

--- HabitBuilderApp/Controllers/TodayViewController.cs
+++ HabitBuilderApp/Controllers/TodayViewController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace HabitBuilderApp.Controllers
 {
@@ -12,6 +13,12 @@ namespace HabitBuilderApp.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("Index", "Home");
         }
 
         // GET: TodayView/Details/5
